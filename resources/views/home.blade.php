bb602e4254b3f5fdfabc17d37a47e4ebb8dde6bf
Sibongiseni Msomi <sibongiseni.msomi@outlook.com>
2017-03-13 19:02:17 +0200

    Dashboard layout, work in progress
    More widgets will be added as needed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,109 +2,223 @@
 
 @section('content')
     @if(Auth::user()->status_is=='Active')
-        @if(Auth::user()->verified==1)
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-10 col-md-offset-1">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">Dashboard</div>
-                            <div class="panel-body">
-                                <div id="accordion">
-                                    <h3>Open</h3>
-                                    <div>
-                                        <table class="table table-striped">
-                                            <thead>
-                                            <tr>
-                                                <th>Reference</th>
-                                                <th>Name</th>
-                                                <th>Host</th>
-                                                <th>Attending</th>
-                                                <th>Start/End Date</th>
-                                                <th>Start/End Time</th>
-                                                <th>Status</th>
-                                                <th>Actions</th>
-                                            </tr>
-                                            </thead>
-                                            <tbody>
-                                            @foreach($allEvents as $event)
-                                                <tr>
-                                                    <td>{{ $event->reference }}</td>
-                                                    <td>{{ $event->name }}</td>
-                                                    <td>{{ $event->host }}</td>
-                                                    @if(count(explode(',', $event->attendees)) > 0 && explode(',', $event->attendees)[0] != "")
-                                                        <td>  {{ count(explode(',', $event->attendees)) }}
-                                                            / {{ $event->number_of_seats }}</td>
-                                                    @else
-                                                        <td>  0
-                                                            / {{ $event->number_of_seats }}</td>
-                                                    @endif
-                                                    <td>{{ $event->start_date }} - {{ $event->end_date }}</td>
-                                                    <td>{{ $event->start_time }} - {{ $event->end_time }}</td>
-                                                    <td>{{ $event->status_is }}</td>
-                                                    <td>
-                                                        <a href="{{ url('view-event', $event->id) }}" class="btn"
-                                                           rel="tooltip" title="View">
-                                                            <b>Show</b>
-                                                        </a>
-                                                        @if($event->status_is == 'FullyBooked')
-                                                            <b>Fully Booked</b>
-                                                        @elseif($event->status_is == 'Open')
-                                                            @if(in_array(Auth::user()->id,explode(',', $event->attendees)))
-                                                                <p>Booking Pending</p>
-                                                            @else
-                                                                <a href="{{ url('booking/create-event-booking/'.$event->id) }}"
-                                                                   class="btn"
-                                                                   rel="tooltip"
-                                                                   title="Edit">
-                                                                    <b>Book</b>
-                                                                </a>
-                                                            @endif
-                                                        @endif
-                                                    </td>
-                                                </tr>
-                                            @endforeach
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                    <h3>Close</h3>
-                                    <div>
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <section class="content-header">
+        <h1>
+          Dashboard
+        </h1>
+        <ol class="breadcrumb">
+          <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+          <li class="active">Dashboard</li>
+        </ol>
+      </section>
 
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+      <!-- Main content -->
+      <section class="content">
+        <!-- /.row -->
+
+        <div class="row">
+          <div class="col-md-6 col-lg-6 col-sm-12">
+            <div class="box">
+              <div class="box-header with-border">
+                <h3 class="box-title">Open Events</h3>
+
+                <div class="box-tools pull-right">
+                  <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                  </button>
+                  @if(Auth::user()->hasRole('admin'))
+                  <div class="btn-group">
+                    <button type="button" class="btn btn-box-tool dropdown-toggle" data-toggle="dropdown">
+                      <i class="ion ion-ios-gear"></i></button>
+                    <ul class="dropdown-menu" role="menu">
+                      <li><a href="{{ url('/events') }}">View All Events</a></li>
+                      <li><a href="{{ url('/events/create') }}">Create an Event</a></li>
+                    </ul>
+                  </div>
+                  @endif
+                  <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
                 </div>
-            </div>
-        @else
-            <div class="row">
-                <div class="col-md-8">
-                    <form class="form-horizontal" role="form" action="{{ url('/verification') }}" method="post">
-                        {!! csrf_field() !!}
-
-                        <div class="form-group{{ $errors->has('verification') ? ' has-error' : '' }}">
-                            <label for="verification" class="col-sm-8 control-label">Verification Code</label>
-                            <div class="col-sm-4">
-                                <input id="verification" name="verification" type="text"
-                                       placeholder="Enter verification code" class="form-control">
-
-                                @if ($errors->has('verification'))
-                                    <span class="help-block">
-                                            <strong>{{ $errors->first('verification') }}</strong>
-                                        </span>
+              </div>
+              <!-- /.box-header -->
+              <div class="box-body">
+                    <ul class="events-list product-list-in-box">
+                    @foreach($allEvents as $event)  
+                      <li class="item">
+                      <div class="row">
+                        <div class="col-md-3">
+                          <div class="event-chart">
+                            @if(count(explode(',', $event->attendees)) > 0 && explode(',', $event->attendees)[0] != "")
+                              <input type="text" class="knob" value="{{($event->number_of_seats - count(explode(',', $event->attendees)))}}" data-thickness="0.2" data-width="100" data-min="0" data-max="{{ $event->number_of_seats }}" data-height="100" data-fgColor="#3c8dbc" data-readonly="true">
+                            @else
+                              <input type="text" class="knob" value="0" data-thickness="0.2" data-width="100" data-min="0" data-max="{{ $event->number_of_seats }}" data-height="100" data-fgColor="#3c8dbc" data-readonly="true">
+                            @endif 
+                              <p class="knob-label">Available Seats</p>
+                          </div>                        
+                        </div>
+                        <div class="col-md-9">
+                          <div class="event-info">
+                          <span class="label label-success pull-right">{{ $event->status_is }}</span>
+                            <h3 class="event-title no-margin">{{ $event->name }} <br><small>with  <strong>{{ $event->host }}</strong></small>
+                            </h3>
+                              <p>Event Reference: <span style="text-decoration: underline;" class="copy">{{ $event->reference }}</span></p>
+                                <span class="event-description">
+                                  <strong>Starts </strong> {{ Carbon\Carbon::parse($event->start_date)->formatLocalized('%A %d %B %Y') }} @ {{ $event->start_time }} <br/>
+                                  <strong>Ends</strong> {{ Carbon\Carbon::parse($event->end_date)->formatLocalized('%A %d %B %Y') }} @ {{ $event->end_time }} <br>
+                                  <strong>Location</strong> {{ $event->address }}
+                                </span>
+                            <p class="pull-right">
+                               <a href="{{ url('view-event', $event->id) }}" class="btn btn-default"
+                                rel="tooltip" title="View">
+                                  <i class="ion ion-ios-calendar-outline"></i> View Event
+                              </a>
+                                @if($event->status_is == 'FullyBooked')
+                                    <button type="button" class="btn btn-disabled" disabled>Fully Booked</button>
+                                @elseif($event->status_is == 'Open')
+                                    @if(in_array(Auth::user()->id,explode(',', $event->attendees)))
+                                    @foreach($bookings as $booking)
+                                      @if($booking->event_id == $event->id AND $booking->status_is == 'Paid')
+                                        <span class="btn btn-success" disabled><i class="ion ion-ios-checkmark-outline"></i> Booking Approved</span>
+                                       @elseif($booking->status_is != 'Paid' AND $booking->event_id == $event->id)
+                                        <span class="btn btn-danger" disabled><i class="ion ion-ios-clock-outline"></i> Booking {{ $booking->status_is }}</span>
+                                       @endif
+                                    @endforeach
+                                    @else
+                                        <a href="{{ url('booking/create-event-booking/'.$event->id) }}"
+                                          class="btn btn-danger btn-sm"
+                                          rel="tooltip"
+                                          title="Edit">
+                                            <i class="ion ion-ios-compose-outline"></i> Book Event
+                                        </a>
+                                    @endif
                                 @endif
-                            </div>
+                            </p>
+                          </div>                        
                         </div>
-                        <div class="form-group">
-                            <div class="col-sm-offset-8 col-sm-8">
-                                <button type="submit" class="btn btn-primary">Submit</button>
-                                <a class="btn btn-default" data-dismiss="modal" href="{{ url('/resend') }}">Resend
-                                    Code</a>
-                            </div>
-                        </div>
-                    </form>
+                      </div>
+                      </li>
+                      <!-- /.item -->
+                  @endforeach
+                    </ul>
+                <div class="row">
+                  <div class="col-md-12">
+                  </div>
+                  <!-- /.col -->
                 </div>
+                <!-- /.row -->
+              </div>
+              <!-- ./box-body -->
             </div>
-        @endif
+            <!-- /.box -->
+          </div>
+          <!-- /.col -->
+
+          <!-- Right col -->
+          <div class="col-lg-6 col-md-6 col-sm-12">
+            <!-- MAP & BOX PANE -->
+            <div class="box box-success">
+              <div class="box-header with-border">
+                <h3 class="box-title">Closed Events</h3>
+
+                <div class="box-tools pull-right">
+                  <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                  </button>
+                  
+                  @if(Auth::user()->hasRole('admin'))
+                  <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                  @endif
+                </div>
+              </div>
+              <!-- /.box-header -->
+              <div class="box-body no-padding">
+                <div class="row">
+                  <div class="col-md-12 col-sm-12">
+
+                  </div>
+                  <!-- /.col -->
+                </div>
+                <!-- /.row -->
+              </div>
+              <!-- /.box-body -->
+            </div>
+            <!-- /.box -->
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
+      </section>
+      <!-- /.content -->
+    </div>
     @endif
+
 @endsection
+
+@section('javascript')
+    <!-- jQuery Knob -->
+    {!! Html::script('plugins/knob/jquery.knob.js') !!}
+    <script>
+        $(function() {
+            /* jQueryKnob */
+            $(".knob").knob({
+                /*change : function (value) {
+                 //console.log("change : " + value);
+                 },
+                 release : function (value) {
+                 console.log("release : " + value);
+                 },
+                 cancel : function () {
+                 console.log("cancel : " + this.value);
+                 },*/
+                draw: function() {
+
+                    // "tron" case
+                    if (this.$.data('skin') == 'tron') {
+
+                        var a = this.angle(this.cv) // Angle
+                            ,
+                            sa = this.startAngle // Previous start angle
+                            ,
+                            sat = this.startAngle // Start angle
+                            ,
+                            ea // Previous end angle
+                            , eat = sat + a // End angle
+                            ,
+                            r = true;
+
+                        this.g.lineWidth = this.lineWidth;
+
+                        this.o.cursor &&
+                            (sat = eat - 0.3) &&
+                            (eat = eat + 0.3);
+
+                        if (this.o.displayPrevious) {
+                            ea = this.startAngle + this.angle(this.value);
+                            this.o.cursor &&
+                                (sa = ea - 0.3) &&
+                                (ea = ea + 0.3);
+                            this.g.beginPath();
+                            this.g.strokeStyle = this.previousColor;
+                            this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, sa, ea, false);
+                            this.g.stroke();
+                        }
+
+                        this.g.beginPath();
+                        this.g.strokeStyle = r ? this.o.fgColor : this.fgColor;
+                        this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, sat, eat, false);
+                        this.g.stroke();
+
+                        this.g.lineWidth = 2;
+                        this.g.beginPath();
+                        this.g.strokeStyle = this.o.fgColor;
+                        this.g.arc(this.xy, this.xy, this.radius - this.lineWidth + 1 + this.lineWidth * 2 / 3, 0, 2 * Math.PI, false);
+                        this.g.stroke();
+
+                        return false;
+                    }
+                }
+            });
+            /* END JQUERY KNOB */
+        });
+    </script>
+@stop
