4634474b040716bc6ba8268d1b8d3e2d54911a9f
Sibongiseni Msomi <sibongiseni.msomi@outlook.com>
2017-03-13 18:58:01 +0200

    Fixed Registration Issues
    Registration Section on Homepage main site has been fixed

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -40,7 +40,7 @@
         <figure class="logo animated fadeInDown delay-07s">
             <a href="#"><img src="img/logo.png" alt=""></a>
         </figure>
-        <h1 class="animated fadeInDown delay-07s">Welcom To GoForex Wealth Creation</h1>
+        <h1 class="animated fadeInDown delay-07s">Welcome To GoForex Wealth Creation</h1>
         <ul class="we-create animated fadeInUp delay-1s">
             <li>One of the leading forex mentorship companies.</li>
         </ul>
@@ -432,99 +432,97 @@
 
             </div>
             <div class="col-lg-6 col-sm-5 wow fadeInUp delay-05s">
-                <div class="form" method="POST" action="{{ route('register') }}">
+                <div id="sendmessage">Your message has been sent. Thank you!</div>
+                <div id="errormessage"></div>
+                <h1 class="animated fadeInDown delay-07s">Join Us Now</h1><br/>
+                <form role="form" class="contactForm" method="POST" action="{{ route('register') }}">
+                    {{ csrf_field() }}
+                    <div class="form-group{{ $errors->has('firstname') ? ' has-error' : '' }}">
+                        <input id="firstname" type="text" class="form-control input-text" name="firstname"
+                                value="{{ old('firstname') }}" required autofocus  placeholder="First Name">
 
-                    <div id="sendmessage">Your message has been sent. Thank you!</div>
-                    <div id="errormessage"></div>
-                    <h1 class="animated fadeInDown delay-07s">Join Us Now</h1><br/>
-                    <form action="" method="post" role="form" class="contactForm">
-                        <div class="form-group{{ $errors->has('firstname') ? ' has-error' : '' }}">
-                            <input id="firstname" type="text" class="form-control input-text" name="firstname"
-                                   value="{{ old('firstname') }}" required autofocus  placeholder="First Name">
+                        @if ($errors->has('firstname'))
+                            <span class="help-block">
+                                    <strong>{{ $errors->first('firstname') }}</strong>
+                                </span>
+                        @endif
+                    </div>
 
-                            @if ($errors->has('firstname'))
-                                <span class="help-block">
-                                        <strong>{{ $errors->first('firstname') }}</strong>
-                                    </span>
-                            @endif
-                        </div>
+                    <div class="form-group{{ $errors->has('lastname') ? ' has-error' : '' }}">
+                        <input id="lastname" type="text" class="form-control input-text" name="lastname"
+                                value="{{ old('lastname') }}" required autofocus placeholder="Last Name ">
 
-                        <div class="form-group{{ $errors->has('lastname') ? ' has-error' : '' }}">
-                            <input id="lastname" type="text" class="form-control input-text" name="lastname"
-                                   value="{{ old('lastname') }}" required autofocus placeholder="Last Name ">
+                        @if ($errors->has('lastname'))
+                            <span class="help-block">
+                                    <strong>{{ $errors->first('lastname') }}</strong>
+                                </span>
+                        @endif
+                    </div>
 
-                            @if ($errors->has('lastname'))
-                                <span class="help-block">
-                                        <strong>{{ $errors->first('lastname') }}</strong>
-                                    </span>
-                            @endif
-                        </div>
+                    <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
+                        <input id="username" type="text" class="form-control input-text" name="username"
+                                value="{{ old('username') }}" required autofocus placeholder="Username">
 
-                        <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
-                            <input id="username" type="text" class="form-control input-text" name="username"
-                                   value="{{ old('username') }}" required autofocus placeholder="Username">
+                        @if ($errors->has('username'))
+                            <span class="help-block">
+                                    <strong>{{ $errors->first('username') }}</strong>
+                                </span>
+                        @endif
+                    </div>
 
-                            @if ($errors->has('username'))
-                                <span class="help-block">
-                                        <strong>{{ $errors->first('username') }}</strong>
-                                    </span>
-                            @endif
-                        </div>
+                    <div class="form-group{{ $errors->has('cell') ? ' has-error' : '' }}">
+                        <input id="cell" type="text" class="form-control input-text" name="cell" value="{{ old('cell') }}"
+                                required autofocu placeholder="Cell Number"s>
 
-                        <div class="form-group{{ $errors->has('cell') ? ' has-error' : '' }}">
-                            <input id="cell" type="text" class="form-control input-text" name="cell" value="{{ old('cell') }}"
-                                   required autofocu placeholder="Cell Number"s>
+                        @if ($errors->has('cell'))
+                            <span class="help-block">
+                                    <strong>{{ $errors->first('cell') }}</strong>
+                                </span>
+                        @endif
+                    </div>
 
-                            @if ($errors->has('cell'))
-                                <span class="help-block">
-                                        <strong>{{ $errors->first('cell') }}</strong>
-                                    </span>
-                            @endif
-                        </div>
+                    <div class="form-group{{ $errors->has('location') ? ' has-error' : '' }}">
+                        <input id="location" type="text" class="form-control input-text" name="location"
+                                value="{{ old('location') }}" required autofocus placeholder="Location">
 
-                        <div class="form-group{{ $errors->has('location') ? ' has-error' : '' }}">
-                            <input id="location" type="text" class="form-control input-text" name="location"
-                                   value="{{ old('location') }}" required autofocus placeholder="Location">
+                        @if ($errors->has('location'))
+                            <span class="help-block">
+                                    <strong>{{ $errors->first('location') }}</strong>
+                                </span>
+                        @endif
+                    </div>
 
-                            @if ($errors->has('location'))
-                                <span class="help-block">
-                                        <strong>{{ $errors->first('location') }}</strong>
-                                    </span>
-                            @endif
-                        </div>
+                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                        <input id="email" type="email" class="form-control input-text" name="email" value="{{ old('email') }}"
+                                required placeholder="Email Address">
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <input id="email" type="email" class="form-control input-text" name="email" value="{{ old('email') }}"
-                                   required placeholder="Email Address">
+                        @if ($errors->has('email'))
+                            <span class="help-block">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </span>
+                        @endif
+                    </div>
 
-                            @if ($errors->has('email'))
-                                <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                            @endif
-                        </div>
+                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                        <input id="password" type="password" class="form-control input-text" name="password" required placeholder="Password">
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <input id="password" type="password" class="form-control input-text" name="password" required placeholder="Password">
+                        @if ($errors->has('password'))
+                            <span class="help-block">
+                                    <strong>{{ $errors->first('password') }}</strong>
+                                </span>
+                        @endif
+                    </div>
 
-                            @if ($errors->has('password'))
-                                <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                            @endif
-                        </div>
-
-                        <div class="form-group">
-                                <input id="password-confirm" type="password" class="form-control input-text"
-                                       name="password_confirmation" required placeholder="Password Confirm">
-                        </div>
+                    <div class="form-group">
+                            <input id="password-confirm" type="password" class="form-control input-text"
+                                    name="password_confirmation" required placeholder="Password Confirm">
+                    </div>
 
 
-                        <div class="text-center">
-                            <button type="submit" class="input-btn">Join</button>
-                        </div>
-                    </form>
-                </div>
+                    <div class="text-center">
+                        <button type="submit" class="input-btn">Join</button>
+                    </div>
+                </form>
             </div>
         </div>
     </section>
