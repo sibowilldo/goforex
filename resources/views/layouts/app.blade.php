cda7f6ed707bebc5ec7a810d190bc267d34cfa43
Sibongiseni Msomi <sibongiseni.msomi@outlook.com>
2017-03-13 19:00:20 +0200

    Admin Theme on App Blade
    Admin theme on app blade, subject to approval...
    Added @yield('javascript') for page specific JS and to limit number of
    script on full site

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,24 @@
 {{ Html::style('css/app.css') }}
 {{ Html::style('css/font-awesome.min.css') }}
 {{ Html::style('css/jquery-ui.css') }}
+  <!-- Bootstrap 3.3.6 -->
+{{ Html::style('bootstrap/css/bootstrap.min.css') }}
+  <!-- Ionicons -->
+{{ Html::style('https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css') }}
+  <!-- jvectormap -->
+{{ Html::style('plugins/jvectormap/jquery-jvectormap-1.2.2.css') }}
+  <!-- Theme style -->
+{{ Html::style('css/AdminLTE.min.css') }}
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+{{ Html::style('css/skins/skin-gold.css') }}
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
 
 
 <!-- Scripts -->
@@ -23,77 +41,135 @@
         ]) !!};
     </script>
 </head>
-<body>
+<body class="hold-transition skin-gold sidebar-mini">
 
 <div id="app">
     @include('flash::message')
-    <nav class="navbar navbar-default navbar-static-top">
-        <div class="container">
-            <div class="navbar-header">
+    <div class="wrapper">
 
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                        data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
+  <header class="main-header">
 
-                <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-            </div>
+    <!-- Logo -->
+    <a href="/home" class="logo">
+      <!-- logo for regular state and mobile devices -->
+      <span class="logo-lg ogo-mini">GoForex</span>
+    </a>
 
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    &nbsp;
-                </ul>
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top">
+      <!-- Sidebar toggle button-->
+      <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+        <span class="sr-only">Toggle navigation</span>
+      </a>
+      <!-- Navbar Right Menu -->
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+          <!-- User Account: style can be found in dropdown.less -->
+          <li class="dropdown user user-menu">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+              <img src="/img/small-logo.png" class="user-image" alt="{{ Auth::user()->firstname }}">
+              <span class="hidden-xs">{{ Auth::user()->firstname }}</span>
+            </a>
+            <ul class="dropdown-menu">
+              <!-- User image -->
+              <li class="user-header">
+                <img src="/img/small-logo.png" class="/img-circle" alt="User Image">
 
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links -->
-                    @if (Auth::guest())
-                        <li><a href="{{ route('login') }}">Login</a></li>
-                        <li><a href="{{ route('register') }}">Register</a></li>
-                    @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                               aria-expanded="false">
-                                {{ Auth::user()->username }} <span class="caret"></span>
-                            </a>
+                <p>
+                  {{ Auth::user()->firstname }} {{ Auth::user()->lastname }}
+                  <small>Member since: {{ Auth::user()->created_at->formatLocalized('%A, %d %B %Y') }}</small>
+                </p>
+              </li>
+              <!-- Menu Footer-->
+              <li class="user-footer">
+                <div class="pull-left">
+                  <a href="#" class="btn btn-default btn-flat">Profile</a>
+                </div>
+                <div class="pull-right">
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                        {{ csrf_field() }}
+                        <button type="submit" class="btn btn-default btn-flat">Sign out</button>
+                    </form>
+                </div>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
 
-                            <ul class="dropdown-menu" role="menu">
-                                <li>
-                                    <a href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        Logout
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                          style="display: none;">
-                                        {{ csrf_field() }}
-                                    </form>
-                                </li>
-                            </ul>
-                        </li>
-                    @endif
-                </ul>
-            </div>
-        </div>
     </nav>
+  </header>
+  <!-- Left side column. contains the logo and sidebar -->
+  <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="/img/logo.png" class="/img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <p>{{ Auth::user()->firstname }} {{ Auth::user()->lastname }}</p>
+          <a href="#"><i class="fa fa-circle text-success"></i> {{ Auth::user()->status_is }}</a>
+        </div>
+      </div>
+      <!-- search form -->
+      <form action="#" method="get" class="sidebar-form">
+        <div class="input-group">
+          <input type="text" name="q" class="form-control" placeholder="Search...">
+              <span class="input-group-btn">
+                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+                </button>
+              </span>
+        </div>
+      </form>
+      <!-- /.search form -->
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu">
+        <li class="header">Main Navigation</li>
+        <li>
+          <a href="/home">
+            <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+          </a>
+        </li>
+        <li>
+          <a href="{{ url('/events')}}">
+            <i class="fa fa-calendar"></i>
+            <span>Events</span>
+          </a>
+        </li>
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
 
     @yield('content')
+    
+
+  <footer class="main-footer">
+    <strong>Copyright &copy; {{ \Carbon\Carbon::now()->year  }} <a href="/">GoForex</a> Wealth Creation.</strong> All rights
+    reserved.
+  </footer>
+    
+</div>
+<!-- ./wrapper -->
 </div>
 
 <!-- Scripts -->
 
+<!-- jQuery 2.2.3 -->
+{!! Html::script('plugins/jQuery/jquery-2.2.3.min.js') !!}
+
 {!! Html::script('/js/app.js') !!}
+<!-- AdminLTE App -->
+{!! Html::script('js/app.min.js') !!}
 {!! Html::script('/js/jquery-ui.min.js') !!}
+<!-- Bootstrap 3.3.6 -->
+{!! Html::script('bootstrap/js/bootstrap.min.js') !!}
 {!! Html::script('/js/jquery.zoomooz.min.js') !!}
+
+@yield('javascript')
+
 
 
 <script type="text/javascript" language="javascript">
