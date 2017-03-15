feed03c6aa9f11bfc5b71e5c70b11ded3fe2920d
Bruno Sekhosana <bsekhosana@gmail.com>
2017-03-15 23:27:56 +0200

    added twak.to live chat

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -214,6 +214,18 @@
     $( ".view-event" ).on( "click", function() {
         $( "#dialog-make-booking" ).dialog( "open" );
     });
+
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/58c9b1295b89e2149e17c922/default';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+    })();
+
 </script>
+
 </body>
 </html>
