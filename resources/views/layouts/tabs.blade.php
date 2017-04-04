404aa600ed39f352c74d86b50294251b8f74f62c
kakapa <admin@profutureplayers.com>
2017-04-04 23:19:37 +0200

    Fixed login tab

--- resources/views/layouts/tabs.blade.php
+++ resources/views/layouts/tabs.blade.php
@@ -1,14 +1,14 @@
 <div>
     <!-- Nav tabs -->
     <ul class="nav nav-tabs centred" role="tablist">
-        <li role="presentation" class="active"><a href="#signin" aria-controls="home" role="tab" data-toggle="tab">Sign In</a></li>
+        <li role="presentation" class="active"><a href="#login" aria-controls="home" role="tab" data-toggle="tab">Sign In</a></li>
         <li role="presentation"><a href="#signup" aria-controls="profile" role="tab" data-toggle="tab">Sign Up</a></li>
         <li role="presentation"><a href="#forgot" aria-controls="messages" role="tab" data-toggle="tab">Forgot Password</a></li>
     </ul>
 
     <!-- Tab panes -->
     <div class="tab-content">
-        <div role="tabpanel" class="tab-pane active" id="signin">
+        <div role="tabpanel" class="tab-pane active" id="login">
             @include('layouts.login')
         </div>
         <div role="tabpanel" class="tab-pane" id="signup">
