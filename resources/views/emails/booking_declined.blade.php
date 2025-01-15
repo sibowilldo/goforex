7c604522ac17932e8ca25cfe894c6e87527648e6
Sibongiseni Msomi <sibongiseni.msomi@outlook.com>
2025-01-15 07:17:44 +0200

    What?
    Not sure what's happening here and too busy to check

--- resources/views/emails/booking_declined.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.emails')
-
-@section('content')
-
-    We regret to inform you that your booking of <b>Ref#{{ $booking_ref }}</b> has been declined.<br/><br/>
-
-    If you are still interested in attending the event please create another booking if seats are still available.<br/>
-
-@endsection
--- resources/views/emails/verified.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.emails')
-
-@section('content')
-
-    Your GoForex account has been verified, and you are good to go.
-    <br/>
-
-@endsection
